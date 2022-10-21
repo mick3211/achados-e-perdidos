d8818c3009549817fd6393545e480df1676efc44
Mickael <mickaelfelizardo2008@gmail.com>
2022-10-21 11:28:02 -0300

    correção de medidas

--- src/ui/components/data-dispay/PageTitle/PageTitle.styled.ts
+++ src/ui/components/data-dispay/PageTitle/PageTitle.styled.ts
@@ -1,14 +1,20 @@
 import { styled } from '@mui/system';
 
 export const PageTitleWrapper = styled('div')`
-    margin-top: ${({ theme }) => theme.spacing(7)};
     text-align: center;
+    margin: ${({ theme }) => theme.spacing(3) + ' ' + 0};
+
+    ${({ theme }) => theme.breakpoints.up('md')} {
+        margin: ${({ theme }) => theme.spacing(7) + ' ' + 0};
+    }
 `;
 
 export const TitleStyled = styled('h4')`
     font-size: 1.125rem;
     font-weight: 700;
-    margin-bottom: ${({ theme }) => theme.spacing(0.75)};
+    margin: 0;
+    line-height: 42px;
+    margin-bottom: ${({ theme }) => theme.spacing(1.5)};
 
     ${({ theme }) => theme.breakpoints.up('md')} {
         font-size: 2.25rem;
@@ -19,6 +25,8 @@ export const TitleStyled = styled('h4')`
 export const SubTitleStyled = styled('p')`
     font-size: 0.75rem;
     font-weight: 400;
+    margin: 0;
+    line-height: 21px;
 
     ${({ theme }) => theme.breakpoints.up('md')} {
         font-size: 1.125rem;
